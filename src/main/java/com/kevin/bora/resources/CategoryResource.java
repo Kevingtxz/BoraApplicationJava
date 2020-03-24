bd35b325b8b70e1a141e0a167b2602d1277d3097
Kevin <kevingtxz@gmail.com>
2020-03-24 19:36:41 -0300

    First Class Test

--- src/main/java/com/kevin/bora/resources/CategoryResource.java
+++ src/main/java/com/kevin/bora/resources/CategoryResource.java
@@ -1,15 +1,29 @@
 package com.kevin.bora.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kevin.bora.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Sport");
+		Category cat2 = new Category(2, "Party");
+
+		List<Category> listCategories = new ArrayList<>();
+		
+		listCategories.add(cat1);
+		listCategories.add(cat2);
+		
+		return listCategories;
 	}
 }
