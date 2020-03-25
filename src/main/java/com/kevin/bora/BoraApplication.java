f90686a8d3ab3a81ce67fdd03de5d0a17d565d6d
Kevin <kevingtxz@gmail.com>
2020-03-24 21:07:32 -0300

    Instance

--- src/main/java/com/kevin/bora/BoraApplication.java
+++ src/main/java/com/kevin/bora/BoraApplication.java
@@ -1,13 +1,31 @@
 package com.kevin.bora;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BoraApplication {
+import com.kevin.bora.domain.Category;
+import com.kevin.bora.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class BoraApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BoraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Sport");
+		Category cat2 = new Category(null, "Party");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
