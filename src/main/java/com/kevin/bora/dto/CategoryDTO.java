b5e9ef60624750a0c304ca54cc5f591de1a1a8a1
Kevin <kevingtxz@gmail.com>
2020-03-26 13:52:21 -0300

    Bean Validation Category

--- src/main/java/com/kevin/bora/dto/CategoryDTO.java
+++ src/main/java/com/kevin/bora/dto/CategoryDTO.java
@@ -2,12 +2,20 @@ package com.kevin.bora.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.kevin.bora.domain.Category;
 
 public class CategoryDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=30, message="O tamanho deve ser entre 5 e 30 caracteres")
 	private String name;
 	
 	public CategoryDTO() {
--- src/main/java/com/kevin/bora/services/CategoryService.java
+++ src/main/java/com/kevin/bora/services/CategoryService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kevin.bora.domain.Category;
+import com.kevin.bora.dto.CategoryDTO;
 import com.kevin.bora.repositories.CategoryRepository;
 import com.kevin.bora.services.exceptions.DataIntegrityException;
 import com.kevin.bora.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoryService {
 	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Category fromDTO(CategoryDTO objDto) {
+		return new Category(objDto.getId(), objDto.getName());
 	}
 }
